8a0e50798d80f6849ab9d0824d1209af794520f2
Tashi Dendup <tashi.dendup@dcpl.bt>
2020-03-23 17:16:05 +0600

    Insert query for guest and guestlog

--- src/bt/gov/moh/eet/dao/GuestDao.java
+++ src/bt/gov/moh/eet/dao/GuestDao.java
@@ -1,31 +1,32 @@
 package bt.gov.moh.eet.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 import bt.gov.moh.eet.dto.GuestDTO;
 import bt.gov.moh.eet.dto.GuestLogDTO;
 import bt.gov.moh.eet.util.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class GuestDao {
 	public GuestDTO fetchGuestDetail(Integer guestID) throws Exception {
 		Connection conn = null;
 		PreparedStatement pst = null;
 		ResultSet rs = null;
-		GuestDTO vo = new  GuestDTO();
-		
+		GuestDTO vo = new GuestDTO();
+
 		try {
 			conn = ConnectionManager.getConnection();
 			pst = conn.prepareStatement(GET_GUEST_DETAILS);
 			pst.setInt(1, guestID);
 			rs = pst.executeQuery();
-			
-			if(rs.getInt("rowCount") > 0) {
+
+			if (rs.getInt("rowCount") > 0) {
 				pst.close();
 				rs.close();
-				
-				while(rs.next()) {
+
+				while (rs.next()) {
 					vo.setGuest_id(rs.getInt("guest_id"));
 					vo.setIdentification_no(rs.getInt("identification_no"));
 					vo.setIdentification_type_id(rs.getInt("identification_type_id"));
@@ -44,7 +45,7 @@ public class GuestDao {
 		} finally {
 			ConnectionManager.close(conn, rs, pst);
 		}
-		
+
 		return vo;
 	}
 
@@ -52,20 +53,20 @@ public class GuestDao {
 		Connection conn = null;
 		PreparedStatement pst = null;
 		ResultSet rs = null;
-		GuestLogDTO vo = new  GuestLogDTO();
-		
+		GuestLogDTO vo = new GuestLogDTO();
+
 		try {
 			conn = ConnectionManager.getConnection();
 			pst = conn.prepareStatement(GET_LAST_GUEST_DETAILS);
 			pst.setInt(1, guest_id);
 			rs = pst.executeQuery();
-			
+
 			int i = 0;
-			if(rs.getInt("rowCount") > i) {
+			if (rs.getInt("rowCount") > i) {
 				pst.close();
 				rs.close();
-				
-				while(rs.next()) {
+
+				while (rs.next()) {
 					vo.setAlert_remarks(rs.getString("alert_remarks"));
 					vo.setAlert_update_time(rs.getDate("alert_update_time"));
 					vo.setAlert_flag(rs.getString("alert_flag").charAt(i));
@@ -87,11 +88,79 @@ public class GuestDao {
 		} finally {
 			ConnectionManager.close(conn, rs, pst);
 		}
-		
+
 		return vo;
 	}
-	
+
+	public String addGuest(GuestDTO dto, Connection conn) {
+		PreparedStatement pst = null;
+		String result = "GUEST_ADD_FAILURE";
+
+		try {
+			pst = conn.prepareStatement(INSERT_INTO_GUEST);
+			pst.setInt(1, dto.getIdentification_type_id());
+			pst.setInt(2, dto.getNationality_id());
+			pst.setString(3, dto.getGuest_name());
+			pst.setString(4, dto.getGender().toString());
+			pst.setInt(5, dto.getAge());
+			pst.setString(6, dto.getPresent_address());
+			pst.setInt(7, dto.getContact_no());
+			pst.setString(8, dto.getResiding_across_border().toString());
+
+			int count = pst.executeUpdate();
+
+			if (count > 0) {
+				result = "GUEST_ADD_SUCCESS";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = "GUEST_ADD_FAILURE";
+		} finally {
+			ConnectionManager.close(null, null, pst);
+		}
+		return result;
+	}
+
+	public String addGuestLog(GuestLogDTO dto, Connection conn) {
+		PreparedStatement pst = null;
+		String result = "GUESTLOG_ADD_FAILURE";
+
+		try {
+			pst = conn.prepareStatement(INSERT_INTO_GUESTLOG);
+			pst.setInt(1, dto.getGuest_id());
+			pst.setInt(2, dto.getTemperature());
+			pst.setString(3, dto.getEntry_or_exit());
+			pst.setDate(4, (Date) dto.getTransaction_date_time());
+			pst.setInt(5, dto.getReason_id());
+			pst.setInt(6, dto.getRequested_gate_id());
+			pst.setString(7, dto.getReason());
+			pst.setInt(8, dto.getGate_id());
+			pst.setString(9, dto.getCreated_by());
+			pst.setDate(10, (Date) dto.getCreated_on());
+			pst.setInt(11, dto.getAlert_flag());
+
+			int count = pst.executeUpdate();
+
+			if (count > 0) {
+				result = "GUESTLOG_ADD_SUCCESS";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = "GUESTLOG_ADD_FAILURE";
+		} finally {
+			ConnectionManager.close(null, null, pst);
+		}
+		return result;
+	}
+
 	private static final String GET_GUEST_DETAILS = "SELECT * FROM guests WHERE cid=?";
-	private static final String GET_LAST_GUEST_DETAILS = "SELECT * FROM guestlog\n" +
-            "WHERE guest_id = ? order by entry_or_exit desc limit 1";
+	private static final String GET_LAST_GUEST_DETAILS = "SELECT * FROM guestlog\n"
+			+ "WHERE guest_id = ? order by entry_or_exit desc limit 1";
+	private static final String INSERT_INTO_GUEST = "insert into guests(identification_no,\n"
+			+ "identification_type_id,\n" + "nationality_id,\n" + "guest_name,\n" + "gender,\n" + "age,\n"
+			+ "present_address,\n" + "contact_no,\n" + "residing_across_border)\n" + "values (?, ?, ?, ?, ?, ?, ?, ?);";
+	private static final String INSERT_INTO_GUESTLOG = "insert into guestlog(guest_id,\n" + "temperature,\n"
+			+ "entry_or_exit,\n" + "transaction_date_time,\n" + "reason_id,\n" + "requested_gate_id,\n" + "reason,\n"
+			+ "gate_id,\n" + "created_by,\n" + "created_on,\n" + "alert_flag)\n"
+			+ "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 }
